c78686b4c38704fa4e6d45fda8419a4fbcb2cdcd
Fabien V <fabien.vermeiren@gmail.com>
2022-05-23 14:55:18 +0200

    add back template

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/admin', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('back/base.html.twig', [
+            'category' => 'Admin',
+            'page' => 'Dashboard',
+        ]);
+    }
+}
