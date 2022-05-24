89097ba60cae3362538d4a822a5240e362f0747e
Fabien V <fabien.vermeiren@gmail.com>
2022-05-24 09:38:37 +0200

    basic crud property

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,40 +4,87 @@ namespace App\Controller;
 
 use App\Entity\Property;
 use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/property')]
 class PropertyController extends AbstractController
+
 {
-//    #[Route('/property', name: 'app_property')]
-//    public function index(): Response
-//    {
-//        return $this->render('property/index.html.twig', [
-//            'controller_name' => 'PropertyController',
-//        ]);
-//    }
-    #[Route('/admin/property/new', name: 'app_new_property')]
-    public function newProperty(Request $request): Response
+    private $category = 'Property';
+
+
+    #[Route('/', name: 'app_property_index', methods: ['GET'])]
+    public function index(PropertyRepository $propertyRepository): Response
+    {
+        return $this->render('back/property/index.html.twig', [
+            'page' => 'List',
+            'category' => $this->category,
+            'properties' => $propertyRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_property_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, PropertyRepository $propertyRepository): Response
     {
         $property = new Property();
         $form = $this->createForm(PropertyType::class, $property);
-
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $property = $form->getData();
-            dump($property);
-            // ... perform some action, such as saving the task to the database
 
-            return $this->redirectToRoute('task_success');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $propertyRepository->add($property, true);
+
+            return $this->redirectToRoute('app_property_index', [], Response::HTTP_SEE_OTHER);
         }
-        return $this->renderForm('back/property/new_property.html.twig', [
-            'category' => 'Property',
-            'page' => 'Create New Property',
+
+        return $this->renderForm('back/property/new.html.twig', [
+            'page' => 'New',
+            'category' => $this->category,
+            'property' => $property,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_property_show', methods: ['GET'])]
+    public function show(Property $property): Response
+    {
+        return $this->render('back/property/show.html.twig', [
+            'page' => 'Details',
+            'category' => $this->category,
+            'property' => $property,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_property_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Property $property, PropertyRepository $propertyRepository): Response
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $propertyRepository->add($property, true);
+
+            return $this->redirectToRoute('app_property_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/property/edit.html.twig', [
+            'page' => 'Edit',
+            'category' => $this->category,
+            'property' => $property,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_property_delete', methods: ['POST'])]
+    public function delete(Request $request, Property $property, PropertyRepository $propertyRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$property->getId(), $request->request->get('_token'))) {
+            $propertyRepository->remove($property, true);
+        }
+
+        return $this->redirectToRoute('app_property_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -12,12 +12,12 @@ use App\Config\PropertyEnumSpaces;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PropertyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+//        TODO: add quill or tinymce
         $builder
             ->add('title',TextType::class,[
                 'trim' => true,
@@ -29,7 +29,7 @@ class PropertyType extends AbstractType
                 ])
             ->add('bedrooms')
             ->add('min_price',MoneyType::class,[
-                'currency' => 'THB',
+                'currency' => '',
                 'html5' => true,
                 'label' => 'Minimum Price',
             ])
@@ -42,7 +42,6 @@ class PropertyType extends AbstractType
             ->add('gym',EnumType::class,[
                 'class' => PropertyEnumSpaces::class
             ])
-            ->add('save', SubmitType::class)
             ;
     }
 
