92e386d959bdd277a885915ebb672623048501c5
Fabien V <fabien.vermeiren@gmail.com>
2022-06-01 12:17:10 +0200

    install symfony easyadmin

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -6,6 +6,7 @@ use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class BookingType extends AbstractType
 {
