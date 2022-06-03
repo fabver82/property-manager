77848298ef8e67ce75a43dbd438f46c118740ea5
Fabien V <fabien.vermeiren@gmail.com>
2022-06-03 14:15:28 +0200

    dynamic menu on front

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Page;
+use App\Entity\Settings;
+use App\Repository\PageRepository;
+use App\Repository\SettingsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_front')]
-    public function index(): Response
+    public function index(SettingsRepository $settingsRepository, PageRepository $pageRepository): Response
     {
+        $pages = $pageRepository->findAll();
+        $settings = $settingsRepository->find(1);
         return $this->render('front/base.html.twig', [
             'controller_name' => 'FrontController',
+            'settings'=>$settings,
+            'pages'=>$pages,
+        ]);
+    }
+    #[Route('/{title}', name: 'app_page')]
+    public function page(SettingsRepository $settingsRepository, PageRepository $pageRepository,Page $page): Response
+    {
+//        $pages = $pageRepository->findOneBy('landing_title'=> $page);
+        $settings = $settingsRepository->find(1);
+//        TODO:link to right template, and create the template
+        return $this->render('front/base.html.twig', [
+            'controller_name' => 'FrontController',
+            'settings'=>$settings,
+            'page'=>$page,
         ]);
     }
 }
