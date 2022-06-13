6d220dfd91764c87a7fb8819d7c7bde985a152df
Fabien V <fabien.vermeiren@gmail.com>
2022-06-13 16:55:12 +0200

    add user form on admin page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,43 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Entity\User;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    #[Route('admin/profile', name: 'app_profile')]
     public function index(): Response
     {
-        return $this->render('front/profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+        $user = $this->getUser();
+        dump($user);
+
+        return $this->render('back/user/profile.html.twig', [
+            'pageBC' => 'Profile',
+            'categoryBC' => 'Users',
+            'user' => $user,
+        ]);
+    }
+    #[Route('admin/profile/edit/{username}', name: 'app_edit_profile', methods: ['GET', 'POST'])]
+    public function edit(Request $request,User $user,UserRepository $userRepo ): Response
+    {
+//        $user = $this->getUser();
+        dump($user);
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepo->add($user, true);
+
+            return $this->redirectToRoute('app_profile', ['username'=>$user->getUsername()], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('back/user/edit.html.twig', [
+            'pageBC' => 'Profile',
+            'categoryBC' => 'Users',
+            'user' => $user,
+            'form' =>$form->createView(),
         ]);
     }
 }
